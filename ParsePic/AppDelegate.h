1eb80a383c0b3b84eeddf6c4a068198145a0b61b
Ashok Gelal <ashokgelal@gmail.com>
2012-05-09 00:24:45 -0600

    Initial Commit

---
+++ ParsePic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParsePic
+//
+//  Created by Ashok Gelal on 5/9/12.
+//  Copyright (c) 2012 Ashok Gelal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParsePic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParsePic
+//
+//  Created by Ashok Gelal on 5/9/12.
+//  Copyright (c) 2012 Ashok Gelal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
